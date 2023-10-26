5879264b7b3ff185c86d22d1f3a2c49da6d42e0f
Spectrum3847 <spectrum3847@gmail.com>
2023-10-25 19:40:37 -0500

    Update wpilib.rb to 2023.4.3 for ARM

--- Casks/wpilib.rb
+++ Casks/wpilib.rb
@@ -1,9 +1,9 @@
 cask 'wpilib' do
-  version '2021.2.2'
-  sha256 '11f70c4ddf0ae55701dfddbc19412746d8b538b0397756363ae6b19cdd897c3b'
+  version '2023.4.3'
+  sha256 '8dad67e28a3bdf482979a1fe4c857d52ab36a2fdec56ae05971b7177d5409ae9'
 
   # github.com/wpilibsuite/allwpilib was verified as official when first introduced to the cask
-  url "https://github.com/wpilibsuite/allwpilib/releases/download/v#{version}/WPILib_macOS-#{version}.dmg"
+  url "https://github.com/wpilibsuite/allwpilib/releases/download/v#{version}/WPILib_macOS-Arm64-#{version}.dmg"
   appcast 'https://github.com/wpilibsuite/allwpilib/releases.atom'
   name 'WPILib Suite'
   homepage 'https://wpilib.org/'
@@ -17,16 +17,16 @@ cask 'wpilib' do
 
   preflight do
     system_command 'mkdir', args: ['-p', "#{staged_path}/artifacts"]
-    system_command 'xattr', args: ['-d', 'com.apple.quarantine', "#{staged_path}/WPILib_Mac-#{version}-artifacts.tar.gz"]
-    system_command 'tar', args: ['-zxf', "#{staged_path}/WPILib_Mac-#{version}-artifacts.tar.gz", '-C', "#{staged_path}/artifacts"]
+    system_command 'xattr', args: ['-d', 'com.apple.quarantine', "#{staged_path}/WPILib_MacArm-#{version}-artifacts.tar.gz"]
+    system_command 'tar', args: ['-zxf', "#{staged_path}/WPILib_MacArm-#{version}-artifacts.tar.gz", '-C', "#{staged_path}/artifacts"]
 
-    system_command '/usr/bin/python', args: ["#{staged_path}/artifacts/tools/ToolsUpdater.py"]
+    system_command '/usr/bin/python3', args: ["#{staged_path}/artifacts/tools/ToolsUpdater.py"]
 
     Dir.glob("#{staged_path}/artifacts/vsCodeExtensions/*.vsix") do |vsix_file|
       system_command "#{ENV['HOMEBREW_PREFIX']}/bin/code", args: ['--install-extension', vsix_file]
     end
 
-    system_command 'rm', args: ["#{staged_path}/WPILib_Mac-#{version}-artifacts.tar.gz"]
+    system_command 'rm', args: ["#{staged_path}/WPILib_MacArm-#{version}-artifacts.tar.gz"]
     system_command 'rm', args: ['-rf', "#{staged_path}/WPILibInstaller.app"]
   end
 
